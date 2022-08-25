55add29533c8256646b5c4b31b9a7c2a2def7801
HaJunRyu <hjr4557@naver.com>
2022-08-26 00:32:06 +0900

    뽑기 시 회전효과를 추가해준다

--- src/pages/carousel.tsx
+++ src/pages/carousel.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import affa from '../assets/images/에프파.png';
 import sixtwosevenone from '../assets/images/6271.png';
 import gonggam from '../assets/images/공감해조.png';
@@ -10,6 +10,24 @@ import removedBranding from '../assets/images/removed-브랜딩팀.png';
 import jungsin from '../assets/images/정신머리.png';
 import hMM from '../assets/images/HMM.png';
 import ladder from '../assets/images/Ladder.png';
+import { css } from '@emotion/react';
+
+const carouselArr = [
+  { teamName: 'affa', src: affa, isRemoving: false, removedSrc: removedBranding },
+  {
+    teamName: 'sixtwosevenone',
+    src: sixtwosevenone,
+    isRemoving: false,
+    removedSrc: removedBranding,
+  },
+  { teamName: 'gonggam', src: gonggam, isRemoving: false, removedSrc: removedBranding },
+  { teamName: 'ggirriggirri', src: ggirriggirri, isRemoving: false, removedSrc: removedBranding },
+  { teamName: 'mashupbang', src: mashupbang, isRemoving: false, removedSrc: removedBranding },
+  { teamName: 'branding', src: branding, isRemoving: false, removedSrc: removedBranding },
+  { teamName: 'jungsin', src: jungsin, isRemoving: false, removedSrc: removedBranding },
+  { teamName: 'hMM', src: hMM, isRemoving: false, removedSrc: removedBranding },
+  { teamName: 'ladder', src: ladder, isRemoving: false, removedSrc: removedBranding },
+];
 
 const Container = styled.div`
   display: flex;
@@ -35,51 +53,82 @@ const RemovedTeam = styled.img`
   width: 275px;
   height: 110px;
   border: 2px solid black;
-  animation: down 1s ease-in;
+  animation: down 3s 2s ease-in both;
 `;
 
-const Scene = styled.div`
-  @keyframes carouselRender {
-    0% {
-      transform: translateZ(-1000px);
+const Scene = styled.div<{
+  randomIndex: number;
+  carouselLength: number;
+  isRemoving: boolean;
+  rotateYDeg: number;
+}>`
+  ${({ randomIndex, carouselLength, isRemoving, rotateYDeg }) => css`
+    @keyframes carouselRender {
+      0% {
+        transform: translateZ(-1000px);
+      }
+      50% {
+        transform: translateZ(-5000px);
+      }
+      100% {
+        transform: translateZ(0px);
+      }
     }
-    50% {
-      transform: translateZ(-5000px);
+
+    @keyframes turn {
+      from {
+        transform: rotateY(0deg);
+      }
+      to {
+        transform: rotateY(-360deg);
+      }
     }
-    100% {
-      transform: translateZ(0px);
+
+    @keyframes remove {
+      from {
+        transform: rotateY(0deg);
+      }
+      to {
+        transform: ${`rotateY(-${rotateYDeg}deg)`};
+      }
     }
-  }
 
-  @keyframes turn {
-    from {
-      transform: rotateY(0deg);
+    width: 200px;
+    height: 340px;
+    position: relative;
+    perspective: 1000px;
+    margin: 250px auto;
+
+    & .carousel__wrapper {
+      transform-style: preserve-3d;
+      transition: transform 1s;
+      animation: ${isRemoving ? 'remove 3s both' : 'turn 5s infinite linear 1s'};
     }
-    to {
-      transform: rotateY(-360deg);
+
+    & .carousel {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      transform-style: preserve-3d;
+      animation: carouselRender 1s;
     }
-  }
+  `}
+`;
 
-  width: 200px;
-  height: 340px;
-  position: relative;
-  perspective: 1000px;
-  margin: 250px auto;
+const CarouselCell = styled.img<{ isRemoving: boolean; rotateY: number; translateZ: number }>`
+  ${({ isRemoving, rotateY, translateZ }) => css`
+    @keyframes up {
+      10% {
+        transform: translateY(0px) ${`rotateY(${rotateY}deg) translateZ(${translateZ}px)`};
+      }
+      70% {
+        transform: translateY(0px) ${`rotateY(${rotateY}deg) translateZ(${translateZ + 100}px)`};
+      }
+      100% {
+        transform: translateY(-1000px) ${`rotateY(${rotateY}deg) translateZ(${translateZ + 100}px)`};
+      }
+    }
 
-  & .carousel__wrapper {
-    transform-style: preserve-3d;
-    animation: turn 5s infinite linear 1s;
-  }
-
-  & .carousel {
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    transform-style: preserve-3d;
-    animation: carouselRender 1s;
-  }
-
-  & .carousel__cell {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -92,7 +141,8 @@ const Scene = styled.div`
     font-size: 36px;
     border: 2px solid black;
     transition: transform 1s;
-  }
+    animation: ${isRemoving ? 'up 1s 3.5s both' : ''};
+  `}
 `;
 
 interface CarouselItem {
@@ -103,40 +153,36 @@ interface CarouselItem {
 }
 
 function Carousel() {
-  const [carousel, setCarousel] = useState<CarouselItem[]>([
-    { teamName: 'affa', src: affa, isRemoving: false, removedSrc: removedBranding },
-    {
-      teamName: 'sixtwosevenone',
-      src: sixtwosevenone,
-      isRemoving: false,
-      removedSrc: removedBranding,
-    },
-    { teamName: 'gonggam', src: gonggam, isRemoving: false, removedSrc: removedBranding },
-    { teamName: 'ggirriggirri', src: ggirriggirri, isRemoving: false, removedSrc: removedBranding },
-    { teamName: 'mashupbang', src: mashupbang, isRemoving: false, removedSrc: removedBranding },
-    { teamName: 'branding', src: branding, isRemoving: false, removedSrc: removedBranding },
-    { teamName: 'jungsin', src: jungsin, isRemoving: false, removedSrc: removedBranding },
-    { teamName: 'hMM', src: hMM, isRemoving: false, removedSrc: removedBranding },
-    { teamName: 'ladder', src: ladder, isRemoving: false, removedSrc: removedBranding },
-  ]);
+  const [carousel, setCarousel] = useState<CarouselItem[]>(carouselArr);
 
   const [removedItems, setRemovedItems] = useState<CarouselItem[]>([]);
-
-  const randomIndex = Math.floor(Math.random() * carousel.length);
+  const [isRemoving, setIsRemoving] = useState(false);
+  const [randomIndex, setRandomIndex] = useState(Math.floor(Math.random() * carousel.length));
 
   const handleRandomRemoveCarousel = () => {
     const removeItem = carousel.find((_, index) => index === randomIndex);
     if (!removeItem) return;
+
+    setIsRemoving(true);
 
     setCarousel(pre =>
       pre.map((carouselItem, index) =>
         index === randomIndex ? { ...carouselItem, isRemoving: true } : carouselItem
       )
     );
-    setTimeout(() => setCarousel(pre => pre.filter((_, index) => index !== randomIndex)), 500);
+    setTimeout(() => {
+      setCarousel(carousel.filter((_, index) => index !== randomIndex));
 
+      const randomValue = Math.floor(Math.random() * (carousel.length - 1));
+
+      setIsRemoving(false);
+      setRandomIndex(randomValue);
+    }, 5500);
     setRemovedItems(pre => [...pre, removeItem]);
   };
+
+  const removeItemRotateYDeg = (360 / carousel.length) * randomIndex * (carousel.length + 1);
+
   return (
     <>
       <Container>
@@ -145,7 +191,12 @@ function Carousel() {
             <button onClick={handleRandomRemoveCarousel}>뽑기</button>
           </div>
         </div>
-        <Scene>
+        <Scene
+          randomIndex={randomIndex}
+          carouselLength={carousel.length}
+          rotateYDeg={removeItemRotateYDeg || 360}
+          isRemoving={isRemoving}
+        >
           <div className="carousel__wrapper">
             <div className="carousel">
               {carousel.map(({ teamName, src, isRemoving }, index) => {
@@ -154,14 +205,16 @@ function Carousel() {
                   (180 * 1.35) / 2 / Math.tan(Math.PI / carousel.length)
                 );
                 return (
-                  <img
+                  <CarouselCell
+                    rotateY={rotateY}
+                    translateZ={translateZ}
+                    isRemoving={isRemoving}
                     src={src}
                     alt=""
-                    className="carousel__cell"
                     style={{
                       transform: `rotateY(${rotateY}deg) translateZ(${
                         translateZ > 0 ? translateZ : 100
-                      }px) ${isRemoving ? 'translateY(-1000px)' : ''}`,
+                      }px)`,
                     }}
                     key={`item-${teamName}`}
                   />
